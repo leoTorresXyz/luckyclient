fa1629c5ffdc7a3b598cadc5966f3c15f244b9bf
Leo Torres <leonardo-torres@hotmail.com>
2022-08-01 10:23:45 -0300

    Backend created

---
+++ backend/src/main/java/com/luckyowee/luckyclient/LuckyclientApplication.java
@@ -0,0 +1,13 @@
+package com.luckyowee.luckyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckyclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/luckyowee/luckyclient/LuckyclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckyowee.luckyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckyclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
